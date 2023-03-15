5d660fc698e70f01f9c02e5b5a82981fc351b00c
aelpires <aelpires@gmail.com>
2023-03-15 01:24:44 +0000

    ajustments

--- functions/index.js
+++ functions/index.js
@@ -1,82 +1,69 @@
 const serverless = require('serverless-http');
 const express = require('express');
+const nodemailer = require('nodemailer');
 const cors = require('cors');
 const helmet = require('helmet');
 const createDOMPurify = require('dompurify');
 const { JSDOM } = require('jsdom');
-const aws = require('@aws-sdk/client-ses');
+
 
 const app = express();
 const window = new JSDOM('').window;
 const DOMPurify = createDOMPurify(window);
+const { smtpHost, smtpUser, smtpPass } = process.env;
 
-const ses = new aws.SES({
-  region: 'us-east-1',
+const mailTransport = nodemailer.createTransport({
+  host: smtpHost,
+  port: 465,
+  secure: true,
+  auth: {
+    user: smtpUser,
+    pass: smtpPass,
+  },
 });
 
-const ORIGINS = ['https:/aelpires.com', 'https://www.aelpires.com'];
+const ORIGIN = 'https://aelpires.com';
 const MAX_EMAIL_LENGTH = 512;
 const MAX_MESSAGE_LENGTH = 4096;
-const EMAIL = 'aelpires@gmail.com';
-const FROM_EMAIL = 'mailbot@aelpires.com';
-const EMAIL_PATTERN = /(.+)@(.+){2,}\.(.+){2,}/;
 
 app.use(helmet());
 app.use(express.json());
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!ORIGINS.includes(origin)) {
-        return callback(
-          new Error(`Not allowed by CORS. Origin must be: ${ORIGINS.join(' or ')}`)
-        );
-      }
-
-      return callback(null, true);
-    },
-  })
-);
-app.options('*', cors());
+app.use(cors({ origin: ORIGIN }));
 
 app.post('/message', async (req, res) => {
   try {
     const email = DOMPurify.sanitize(req.body.email);
     const message = DOMPurify.sanitize(req.body.message);
 
+    // Reject unsupported origins
+    if (req.headers.origin !== ORIGIN) {
+      throw new Error(`Unsupported origin: ${req.headers.origin}`);
+    }
+
     // Validate email request
-    if (!email || !EMAIL_PATTERN.test(email)) {
+    if (!email || !/(.+)@(.+){2,}\.(.+){2,}/.test(email)) {
       return res.status(400).json({ error: 'Please enter a valid email address' });
-    }
-
-    if (!message) {
+    } else if (!message) {
       return res.status(400).json({ error: 'Please enter a message' });
-    }
-
-    if (email.length > MAX_EMAIL_LENGTH) {
+    } else if (email.length > MAX_EMAIL_LENGTH) {
       return res.status(400).json({
         error: `Please enter an email fewer than ${MAX_EMAIL_LENGTH} characters`,
       });
-    }
-
-    if (message.length > MAX_MESSAGE_LENGTH) {
+    } else if (message.length > MAX_MESSAGE_LENGTH) {
       return res.status(400).json({
         error: `Please enter a message fewer than ${MAX_MESSAGE_LENGTH} characters`,
       });
     }
 
-    // Send email using AWS SES
-    await ses.sendEmail({
-      Source: `Portfolio <${FROM_EMAIL}>`,
-      Destination: {
-        ToAddresses: [EMAIL],
-      },
-      Message: {
-        Subject: { Data: `New message from ${email}` },
-        Body: {
-          Text: { Data: `From: ${email}\n\n${message}` },
-        },
-      },
-    });
+    // Send email
+    const mailOptions = {
+      from: `Portfolio <mailbot@aelpires.com>`,
+      to: 'aelpires@gmail.com',
+      subject: `New message from ${email}`,
+      text: `From: ${email}\n\n${message}`,
+    };
+
+    await mailTransport.sendMail(mailOptions);
 
     return res.status(200).json({ message: 'Message sent successfully' });
   } catch (error) {
