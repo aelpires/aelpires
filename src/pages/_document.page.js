345b7f8613673e87ba49ba6465a2f40517d8205b
aelpires <aelpires@gmail.com>
2023-03-29 12:56:18 +0100

    import update

--- src/pages/_document.page.js
+++ src/pages/_document.page.js
@@ -1,7 +1,7 @@
 import GothamBook from 'assets/fonts/gotham-book.woff2';
 import GothamMedium from 'assets/fonts/gotham-medium.woff2';
 import { fontStyles, tokenStyles } from 'components/ThemeProvider';
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript, Script } from 'next/document';
 
 export default function Document() {
   return (
